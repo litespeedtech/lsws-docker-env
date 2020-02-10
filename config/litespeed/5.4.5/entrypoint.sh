270ea5952358d2e85282eb1eed68082f710af9e5
Cold-Egg <eleu@litespeedtech.com>
2020-02-10 14:45:14 -0500

    add all

--- config/litespeed/5.4.5/entrypoint.sh
+++ config/litespeed/5.4.5/entrypoint.sh
@@ -4,7 +4,7 @@ wget -P /usr/local/lsws/conf http://license.litespeedtech.com/reseller/trial.key
 
 chown 999:999 /usr/local/lsws/conf -R
 chown 999:1000 /usr/local/lsws/admin/conf -R
-
+$@
 while true; do
 	if [ "$(/usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running with PID *')" = '' ]; then
 		break
