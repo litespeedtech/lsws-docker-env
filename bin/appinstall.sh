ac338f4b7e51bde3b92aba7eefcd412a7a42fa8d
Cold-Egg <eleu@litespeedtech.com>
2020-07-10 15:11:23 +0800

    add ma to script

--- bin/appinstall.sh
+++ bin/appinstall.sh
@@ -2,6 +2,7 @@
 APP_NAME=''
 DOMAIN=''
 EPACE='        '
+SAMPLE=''
 
 echow(){
     FLAG=${1}
@@ -13,7 +14,8 @@ help_message(){
     echo -e "\033[1mOPTIONS\033[0m"
     echow '-A, --app [app_name] -D, --domain [DOMAIN_NAME]'
     echo "${EPACE}${EPACE}Example: appinstall.sh -A wordpress -D example.com"
-    echo "${EPACE}${EPACE}Will install WordPress CMS under the example.com domain"
+    echo "${EPACE}${EPACE}Example: appinstall.sh -A magento -D example.com"
+    echo "${EPACE}${EPACE}Example: appinstall.sh -A magento -S -D example.com"
     echow '-H, --help'
     echo "${EPACE}${EPACE}Display help and exit."
     exit 0
@@ -26,14 +28,44 @@ check_input(){
     fi
 }
 
+set_phpmemory(){
+    if [ "${1}" = 'magento' ]; then 
+        PHP_INI=$(docker-compose exec litespeed su -c "php -i | grep 'Loaded Configuration File' | cut -d' ' -f5 " | tr -d '\r')
+        PHP_MEMORY=$(docker-compose exec litespeed su -c "cat $PHP_INI | grep memory_limit" | tr -d '\r')
+        docker-compose exec litespeed su -c "sed -i 's/^memory_limit.*/memory_limit = 512M/g' $PHP_INI"
+        echo PHP_INI $PHP_INI
+        echo PHP_MEMORY $PHP_MEMORY
+    fi    
+}
+
+revert_phpmemory(){
+    if [ "${1}" = 'magento' ]; then 
+        docker-compose exec litespeed /bin/bash -c "sed -i 's/^memory_limit.*/$PHP_MEMORY/g' $PHP_INI"
+    fi    
+}   
+
+
+install_packages(){
+    if [ "${1}" = 'wordpress' ]; then
+        docker-compose exec litespeed /bin/bash -c "pkginstallctl.sh --package ed"
+        docker-compose exec litespeed /bin/bash -c "pkginstallctl.sh --package unzip"  
+    elif [ "${1}" = 'magento' ]; then
+        docker-compose exec litespeed /bin/bash -c "pkginstallctl.sh --package composer"
+        docker-compose exec litespeed /bin/bash -c "pkginstallctl.sh --package git"
+    fi    
+}
+
 app_download(){
-    docker-compose exec litespeed su -c "appinstallctl.sh --app ${1} --domain ${2}"
+    set_phpmemory ${1}
+    install_packages ${1}
+    docker-compose exec litespeed bash -c "appinstallctl.sh --app ${1} --domain ${2} ${3}"
+    revert_phpmemory ${1}
     bash bin/webadmin.sh -r
     exit 0
 }
 
 main(){
-    app_download ${APP_NAME} ${DOMAIN}
+    app_download "${APP_NAME}" "${DOMAIN}" "${SAMPLE}"
 }
 
 check_input ${1}
@@ -49,7 +81,10 @@ while [ ! -z "${1}" ]; do
         -[dD] | -domain | --domain) shift
             check_input "${1}"
             DOMAIN="${1}"
-            ;;          
+            ;;
+		-[sS] | --sample)
+            SAMPLE='-S'
+            ;;                
         *) 
             help_message
             ;;              
